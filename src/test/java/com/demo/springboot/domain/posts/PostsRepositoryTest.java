8d9667f404e82ec757023cab55c940dd91c52784
Poxios <poxios0310@gmail.com>
2022-05-21 14:46:12 +0900

    working on first service-domain code

---
+++ src/test/java/com/demo/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.demo.springboot.domain.posts;
+
+import com.demo.springboot.web.HelloController;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+
+@RunWith(SpringRunner.class) // JUnit 이외의 실행자 - 여기서는 SpringRunner 스프링 실행자를 실행시킨다.
+@SpringBootTest
+@AutoConfigureMockMvc
+
+public class PostsRepositoryTest  {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void save_and_restore_posts() throws Exception{
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("test@test.com")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
